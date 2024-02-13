87a3576b6cae8a6b39011cc7284dade1510d6e27
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-02-12 21:25:04 -0500

    Mongoose connection and body parser setup

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 
 import express from 'express';
 import routes from './src/routes/crmRoute';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 
 
 // To run express server
 const app = express();
 const PORT = 4000;
+
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb', {
+    useNewUrlParser: true
+});
+
+// body parser setup for json back and forth between db and server
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
 // To run our routes inside of our app
 routes(app);
